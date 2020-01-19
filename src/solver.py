c0887711b68a222100a136c128a74a0479d24a9f
Dhrun Lauwers <dhrun.lauwers@gmail.com>
2020-01-19 03:03:51 -0500

    finished solver

--- src/solver.py
+++ src/solver.py
@@ -11,7 +11,7 @@ board = [
 ]
 
 def print_board(board):
-
+# takes a board, and prints it to console
     for i in range(len(board)):
         if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - -")
@@ -26,21 +26,68 @@ def print_board(board):
                 print(str(board[i][j]) + " ", end="")
 
 def find_empty(board):
-# pick empty square
-
+# takes a board, and returns a tuple representing the position of the first available empty square
     for i in range(len(board)):
         for j in range(len(board[0])):
             if board[i][j] == 0:
                 return (i, j)
+    
+    return None
 
-#print_board(board)
+def valid(board, num, pos):
+#takes a board, value, and position on the board, and checks whether this entry is valid
+    #check row
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
 
-print(find_empty(board))
+    #check col
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+    
+    #check square (3x3)
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
 
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
+
+def solve(board):
+#takes a board, finds the first empty square, and checks whether there is a valid solution
+    find = find_empty(board)
+    if not find:
+        #solution found
+        return True
+    else:
+        #unpack empty
+        row, col = find
+
+    # iterate through possible numbers
+    for i in range(1,10):
+        #if valid, change number and solve again
+        if valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
 
 # try a number
 
 # find one that works
+print_board(board)
+solve(board)
+print("")
+print("")
+print_board(board)
 
 # repeat until no options work
 
