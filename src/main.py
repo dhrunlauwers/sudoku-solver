f3dad80ba401bb06c2e36de1831a0279444c1554
Dhrun Lauwers <dhrun.lauwers@gmail.com>
2020-01-19 20:55:00 -0500

    first step to building ui with pygame

--- src/main.py
+++ src/main.py
@@ -1,19 +1,62 @@
 import solver
+import pygame
 
-board = [
-    [3,0,1,0,0,4,0,5,0],
-    [4,0,7,0,0,0,6,9,0],
-    [0,9,0,0,3,0,0,7,1],
-    [0,2,8,0,1,0,9,0,7],
-    [0,0,0,2,0,7,0,0,0],
-    [1,0,6,0,5,0,2,4,0],
-    [7,5,0,0,6,0,0,2,0],
-    [0,1,3,0,0,0,5,0,9],
-    [0,4,0,5,0,0,7,0,6]
-]
+class Game:
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self.size = self.weight, self.height = 640,400
+    
+    def on_init(self):
+        pygame.init()
+        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._running = True
 
-solver.print_board(board)
-solver.solve(board)
-print("")
-print("")
-solver.print_board(board)
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self._running = False
+
+    def on_loop(self):
+        pass
+
+    def on_render(self):
+        pass
+
+    def on_cleanup(self):
+        pygame.quit()
+    
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+        
+        while(self._running):
+            for event in pygame.event.get():
+                self.on_event(event)
+            
+            self.on_loop()
+            self.on_render()
+        
+        self.on_cleanup()
+
+if __name__ == "__main__":
+    #theGame = Game()
+    #theGame.on_execute()
+    
+
+    board = [
+        [3,0,1,0,0,4,0,5,0],
+        [4,0,7,0,0,0,6,9,0],
+        [0,9,0,0,3,0,0,7,1],
+        [0,2,8,0,1,0,9,0,7],
+        [0,0,0,2,0,7,0,0,0],
+        [1,0,6,0,5,0,2,4,0],
+        [7,5,0,0,6,0,0,2,0],
+        [0,1,3,0,0,0,5,0,9],
+        [0,4,0,5,0,0,7,0,6]
+    ]
+
+    solver.print_board(board)
+    solver.solve(board)
+    print("")
+    print("")
+    solver.print_board(board)
